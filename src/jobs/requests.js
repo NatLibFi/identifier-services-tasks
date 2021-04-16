ed38e52088bf9e817216da2116eea27520a9d6d9
codefeak <codefever17@gmail.com>
2021-04-16 15:02:57 +0300

    debug issn undefined

--- src/jobs/requests.js
+++ src/jobs/requests.js
@@ -108,6 +108,8 @@ export default function (agenda) {
     }
 
     if (type === 'publications') {
+      // eslint-disable-next-line no-console
+      console.log(request);
       await requests.update({path: `requests/${type}/${subtype}/${request.id}`, payload: filteredDoc});
       return logger.log('info', `Background processing State changed to ${state} for${request.id}`);
     }
